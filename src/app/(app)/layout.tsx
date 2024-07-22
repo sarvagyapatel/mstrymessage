6d0ea5431e0205f677f210ce798eb627d1fbb28e
sarvagyapatel <103870100+sarvagyapatel@users.noreply.github.com>
2024-07-22 17:43:37 +0530

    implemented dynamic navbar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import AuthProvider from "@/context/AuthProvider";
-import { Toaster } from "@/components/ui/toaster"
-import './globals.css'
+import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,12 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
         <body className={inter.className}>
+          <Navbar />
           {children}
-          <Toaster />
         </body>
-      </AuthProvider>
     </html>
   );
 }
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -5,88 +5,94 @@ import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/components/ui/use-toast';
 import { signInSchema } from '@/schemas/signInSchema';
-import { verifySchema } from '@/schemas/verifySchema';
-import { ApiResponse } from '@/types/ApiResponse';
 import { zodResolver } from '@hookform/resolvers/zod';
-import axios, { AxiosError } from 'axios';
 import { signIn } from 'next-auth/react';
-import { useParams, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 
 function SignInForm() {
-    const router = useRouter();
-    const { toast } = useToast();
+  const router = useRouter();
+  const { toast } = useToast();
 
 
-    const form = useForm<z.infer<typeof signInSchema>>({
-        resolver: zodResolver(signInSchema),
-    });
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+  });
 
-    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
-       
-      const result = await signIn('credentials', {
-        redirect: false,
-        identifier: data.indentifier,
-        password: data.password
-      })
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
 
-      if(result?.error){
+    const result = await signIn('credentials', {
+      redirect: false,
+      identifier: data.indentifier,
+      password: data.password
+    })
+
+    if (result?.error) {
+      if (result.error === 'CredentialsSignin') {
         toast({
-          title: "Login failed",
-          description: "Incorrect email or password",
-          variant: "destructive"
-        })
+          title: 'Login Failed',
+          description: 'Incorrect username or password',
+          variant: 'destructive',
+        });
+      } else {
+        toast({
+          title: 'Error',
+          description: result.error,
+          variant: 'destructive',
+        });
       }
-
-      console.log(result)
-
-      if(result?.url){
-        router.replace('/dahboard')
-      }
-
     }
-    return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-800">
-            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-                <div className="text-center">
-                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-                        SignIn
-                    </h1>
-                    <p>Enter emai and password</p>
-                </div>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
 
-                        <FormField
-                            name="indentifier"
-                            control={form.control}
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Email</FormLabel>
-                                    <Input {...field} placeholder='email'/>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            name="password"
-                            control={form.control}
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <Input type='password' {...field} placeholder='password' />
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <Button type="submit" className='w-full' >
-                            Submit
-                        </Button>
-                    </form>
-                </Form>
-                {/* <div className="text-center mt-4">
+
+    console.log(result)
+
+    if (result?.url) {
+      router.replace('/dashboard')
+    }
+
+  }
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            SignIn
+          </h1>
+          <p>Enter emai and password</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+
+            <FormField
+              name="indentifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <Input {...field} placeholder='email' />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type='password' {...field} placeholder='password' />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className='w-full' >
+              Submit
+            </Button>
+          </form>
+        </Form>
+        {/* <div className="text-center mt-4">
                     <p>
                         Already a member?{' '}
                         <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">
@@ -94,9 +100,9 @@ function SignInForm() {
                         </Link>
                     </p>
                 </div> */}
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default SignInForm
