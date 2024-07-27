2c1aacc32df9fd9dd7c360a2b2d2d2d2c0e0eb35
sarvagyapatel <103870100+sarvagyapatel@users.noreply.github.com>
2024-07-27 22:03:12 +0530

    patched errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,13 @@ import { User } from 'next-auth';
 
 function Navbar() {
   const { data: session } = useSession();
-  const user : User = session?.user as User;
+  const user: User = session?.user as User;
 
   return (
     <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <a href="#" className="text-xl font-bold mb-4 md:mb-0">
-          Mystry Message
+          Mystery Message
         </a>
         {session ? (
           <>
